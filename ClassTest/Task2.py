cef50060081d80621a3732b9e4c30debead00d0e
Bogd4nB1 <sindbad.long@yandex.ru>
2025-03-05 16:05:16 +0300

    update 3 task

--- ClassTest/Task2.py
+++ ClassTest/Task2.py
@@ -4,6 +4,9 @@ team_2: int = int(input("Qty 2 team: "))
 def competition(team_1, team_2):
     if team_1 == team_2:
         return team_1
-    return team_1 * team_2
+    max_current = team_1 * team_2
+    for i in range(min(team_1, team_2), max_current + 1):
+        if i % team_1 == 0 and i % team_2 == 0:
+            return i
 
 print(competition(team_1, team_2))
